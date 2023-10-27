5c587da0c712e90e697136f2a3dd714285e57614
Techdoc <115435697+Techdoc-ng@users.noreply.github.com>
2023-10-27 09:01:07 -0700

    second  commit

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -3,8 +3,10 @@ const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
 export function CardSkeleton() {
+  const num=[1,2,3,4]
   return (
-    <div
+    num.map((item)=>
+   <div
       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
     >
       <div className="flex p-4">
@@ -14,9 +16,27 @@ export function CardSkeleton() {
       <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
         <div className="h-7 w-20 rounded-md bg-gray-200" />
       </div>
-    </div>
+    </div> 
+     )
+   
   );
 }
+
+// export function CardSkeleton() {
+//   return (
+//     <div
+//       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
+//     >
+//       <div className="flex p-4">
+//         <div className="h-5 w-5 rounded-md bg-gray-200" />
+//         <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
+//       </div>
+//       <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
+//         <div className="h-7 w-20 rounded-md bg-gray-200" />
+//       </div>
+//     </div>
+//   );
+// }
 
 export function CardsSkeleton() {
   return (
